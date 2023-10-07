a3381e7f8ebc075031de8db0f3c10b3c39c4652a
Paul-Nicolas Madelaine <pnm@pnm.tf>
2023-10-07 16:30:02 +0200

    slow job

--- nix/jobs.nix
+++ nix/jobs.nix
@@ -6,7 +6,16 @@
 utils.eachSystem (
   system: let
     pkgs = sources.nixpkgs.legacyPackages.${system};
+    slow = pkgs.runCommand "slow" {} ''
+      echo Hello
+      for i in $(seq 0 1 100); do
+        printf "% 4d%s\n" $i '%'
+        sleep 1
+      done
+      exit 1
+    '';
   in {
     inherit (pkgs) hello;
+    inherit slow;
   }
 )
